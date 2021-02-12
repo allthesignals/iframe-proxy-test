9d50c89877b880d3e6e02772828177b0ea206626
allthesignals <wilburnforce@gmail.com>
2021-02-11 21:20:54 -0500

    validate

--- functions/proxy.js
+++ functions/proxy.js
@@ -1,7 +1,16 @@
 const request = require('superagent');
+const { URL } = require('url');
 
 exports.handler = async (event) => {
   const site = event.queryStringParameters && event.queryStringParameters.site
+  const { origin } = new URL(site);
+
+  if (origin !== 'https://nycplanning.carto.com' && origin !== 'https://nycplanning-web.carto.com') {
+    return {
+      statusCode: 500,
+      body: 'Origin not allowed.',
+    }
+  }
 
   const response = await request
     .get(site)
